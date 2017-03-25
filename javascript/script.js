f14c45448cac410db5630c585036b24ca4c3c5c1
nayak <wjdduq914@gmail.com>
2017-03-25 16:40:41 +0900

    Update. GA

--- javascript/script.js
+++ javascript/script.js
@@ -2,10 +2,12 @@ $(function(){
 	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-64648971-1', 'auto');
+	ga('create', 'UA-96179723-1', 'auto');
 	ga('send', 'pageview');
+
+
 	$(".ready").click(function(){
 		alert("개발 중입니다.");
 	});
