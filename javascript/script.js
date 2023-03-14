f9560e4da11654f012e8b38c4d20a4c234264a6f
nayak <wjdduq914@gmail.com>
2023-03-14 15:40:36 +0900

    feat: ga 갱신

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,8 @@
 $(function(){
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	ga('create', 'UA-96179723-1', 'auto');
-	ga('send', 'pageview');
+	window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-8BXYQ1V3C5');
 
 
 	$(".ready").click(function(){
